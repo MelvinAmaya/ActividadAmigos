1ee0fe1c96fcf56245c1fb74f612347282a40590
MelvinPereira <pereiramelvin78@gmail.com>
2022-05-07 18:07:11 -0600

    Comentarios

--- app/src/main/java/com/example/actividadamigos/MainActivity.kt
+++ app/src/main/java/com/example/actividadamigos/MainActivity.kt
@@ -8,28 +8,42 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    //creamos variable nulas
     private var recyclerView:RecyclerView?=null
     private var recyclerViewAdapter:AddFriends?=null
+    //creamos una lista la cual le enviar datos a la clase Movie_amigos
     private var movieList = mutableListOf<Movie_amigos>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //declaramos la lista como un arrayList
         movieList = ArrayList()
+        //enlazamos los elementos graficos para utilizarlos en la programacion
         recyclerView = findViewById<View>(R.id.recycler_view) as RecyclerView
+        //el adapter sera igual a la clase Addfiends la cual recibe como paramero la lista
         recyclerViewAdapter = AddFriends(movieList)
+        //creamos una varable
         val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+        //igualamos la variable recyclerView a layoutManager
         recyclerView!!.layoutManager = layoutManager
+        //creamos el metoso ClickListener para cuando precionen un rejistro se muestre el titulo del mismo
         recyclerViewAdapter!!.setOnItemClickListener(object : ClickListener<Movie_amigos> {
             override fun onItemClick(data: Movie_amigos) {
+                //enviamos un toast
                 Toast.makeText(this@MainActivity, data.title, Toast.LENGTH_SHORT).show()
             }
         })
+        //enviamos el adapter al recyclerView
         recyclerView!!.adapter = recyclerViewAdapter
+        //llamamos el metodo
         prepareMovie()
     }
 
+    //creamos el metodo
     private fun prepareMovie() {
+        //creamos la variable movie la cual alamcenara los registros
         var movie = Movie_amigos("Joel Cristopher Turcios Turcios", R.drawable.cris,21)
+        //agregamos el registro a la lista
         movieList.add(movie)
         movie = Movie_amigos("Melvin Adiel Vasquez Mejia", R.drawable.adiel, 20)
         movieList.add(movie)
--- app/src/main/java/com/example/actividadamigos/Movie_amigos.kt
+++ app/src/main/java/com/example/actividadamigos/Movie_amigos.kt
@@ -1,3 +1,4 @@
 package com.example.actividadamigos
 
+//recibimos los datos
 data class Movie_amigos(var title: String, var image: Int, var rating: Int)
